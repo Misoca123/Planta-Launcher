bc92bb89a87faf34fb962a6d07950b8e015c041e
Misoca <Minecraft.neviatim@gmail.com>
2024-04-24 03:51:23 +0200

    V1.0.0

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,8 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Chargement en cours", "author": "Misoca123" },
+            { "message": "Bonjour humain", "author": "Misoca123" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
@@ -119,12 +118,12 @@ class Splash {
             this.startLauncher();
         }).catch(e => {
             console.error(e);
-            return this.shutdown("Aucune connexion internet détectée,<br>veuillez réessayer ultérieurement.");
+            return this.shutdown("Impossible de se connecter au serveur,<br>vérifiez votre connexion internet.");
         })
     }
 
     startLauncher() {
-        this.setStatus(`Démarrage du launcher`);
+        this.setStatus(`Démarrage du lanceur`);
         ipcRenderer.send('main-window-open');
         ipcRenderer.send('update-window-close');
     }
